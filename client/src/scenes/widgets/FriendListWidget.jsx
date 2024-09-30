860dae3c80754f33645e25c09789ba174804331a
Kaustubh Dayma <kaustubhdayma@gmail.com>
2024-09-30 11:50:27 +0530

    Deploying Site

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -13,7 +13,7 @@ const FriendListWidget = ({userId}) => {
 
     const getFriends = async () => {
         const response = await fetch(
-            `http://localhost:3001/${userId}/friends`,{
+            `http://https://masnova-the-social-media-website.onrender.com/${userId}/friends`,{
                 method:"GET",
                 headers: {Authoriazation:`Bearer ${token}`}
             }
